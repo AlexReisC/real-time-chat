43b909307e062671542022310ce448ef62970200
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-10-27 10:30:38 -0300

    feat(chat-service): create Room entity

---
+++ chat-service/src/main/java/chat/chat_service/model/Room.java
@@ -0,0 +1,22 @@
+package chat.chat_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collation = "rooms")
+public class Room {
+    @Id
+    private String id;
+    private String title;
+    List<String> members;
+}
