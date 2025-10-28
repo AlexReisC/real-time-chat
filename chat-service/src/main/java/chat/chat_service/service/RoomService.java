935c8d773b2f03bfc243fecf189ddcca51e1c01d
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-10-28 12:13:42 -0300

    feat(chat-service): create RoomService and method for add a user in a room

---
+++ chat-service/src/main/java/chat/chat_service/service/RoomService.java
@@ -0,0 +1,27 @@
+package chat.chat_service.service;
+
+import chat.chat_service.exception.EntityAlreadyExistsException;
+import chat.chat_service.exception.RoomNotFoundException;
+import chat.chat_service.model.Room;
+import chat.chat_service.repository.RoomRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    public void addNewUser(String roomId, String username){
+        Room room = roomRepository.findById(roomId).orElseThrow(() -> new RoomNotFoundException("Sala não encontrada!"));
+        if (room.getMembersUsernames().contains(username)){
+            throw new EntityAlreadyExistsException("Já existe um usuário com este username na sala.");
+        }
+        room.getMembersUsernames().add(username);
+    }
+}
