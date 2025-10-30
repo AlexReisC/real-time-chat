2817c413e73f5fd412a7ee03c3be79efe495d6ec
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-10-30 16:54:57 -0300

    feat(chat-service): add @JsonProperty in ChatMessageDTO and add method for convert dto to Message entity

--- chat-service/src/main/java/chat/chat_service/dto/ChatMessageDTO.java
+++ chat-service/src/main/java/chat/chat_service/dto/ChatMessageDTO.java
@@ -1,12 +1,23 @@
 package chat.chat_service.dto;
 
+import chat.chat_service.model.Message;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.time.Instant;
 
 public record ChatMessageDTO(
-        String type,
-        String roomId,
-        String sender,
-        String content,
-        Instant timestamp
+        @JsonProperty("roomId") String roomId,
+        @JsonProperty("content") String content,
+        @JsonProperty("recipientId") String recipientId,
+        @JsonProperty("timestamp") Instant timestamp
 ) {
+    public Message toEntity(String senderId, String senderUsername) {
+        Message message = new Message();
+        message.setContent(this.content);
+        message.setSenderId(senderId);
+        message.setSenderUsername(senderUsername);
+        message.setRecipientId(this.recipientId);
+        message.setTimestamp(this.timestamp);
+        return message;
+    }
 }
