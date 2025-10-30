6f0a40327d69b727914144cbe8ba466eb6c35c7d
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-10-30 16:53:00 -0300

    feat(chat-service): create DTO for response user notification

---
+++ chat-service/src/main/java/chat/chat_service/dto/UserNotificationResponseDTO.java
@@ -0,0 +1,15 @@
+package chat.chat_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+
+public record UserNotificationResponseDTO(
+        @JsonProperty("type") String type,  // "JOIN" or "LEAVE"
+        @JsonProperty("userId") String userId,
+        @JsonProperty("username") String username,
+        @JsonProperty("roomId") String roomId,
+        @JsonProperty("content") String content,
+        @JsonProperty("timestamp") Instant timestamp
+) {
+}
