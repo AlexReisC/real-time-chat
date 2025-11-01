9375eab2003b700ece3498d69a7c1488a4d603c4
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-01 16:57:04 -0300

    feat(chat-service): create DTO for private messages

---
+++ chat-service/src/main/java/chat/chat_service/dto/PrivateMessageDTO.java
@@ -0,0 +1,14 @@
+package chat.chat_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PrivateMessageDTO(
+        @NotBlank(message = "O ID do recipiente é obrigatório")
+        String recipientId,
+
+        @NotBlank(message = "O conteúdo não pode estar em branco")
+        @Size(max = 1024, message = "A mensagem não pode exceder 1024 caracteres")
+        String content
+) {
+}
