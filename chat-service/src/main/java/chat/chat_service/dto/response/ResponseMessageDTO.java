e883e42a62eeab6fb7448f3f288a291ab68d8e6d
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-02 16:19:09 -0300

    refactor(chat-service): rename from ResumeMessageDTO to ResponseMessageDTO

--- chat-service/src/main/java/chat/chat_service/dto/response/ResponseMessageDTO.java
+++ chat-service/src/main/java/chat/chat_service/dto/response/ResponseMessageDTO.java
@@ -2,10 +2,11 @@ package chat.chat_service.dto.response;
 
 import java.time.Instant;
 
-public record ResumeMessageDTO(
+public record ResponseMessageDTO(
         String id,
         String roomId,
         String senderId,
+        String recipient,
         String content,
         Instant timestamp
 ) {
