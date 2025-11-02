45d76a4a9c033dfbad48e1f4786a0da00b2231f2
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-02 16:20:26 -0300

    feat(chat-service): create method for find private messages in MessageRepository

--- chat-service/src/main/java/chat/chat_service/repository/MessageRepository.java
+++ chat-service/src/main/java/chat/chat_service/repository/MessageRepository.java
@@ -4,9 +4,12 @@ import chat.chat_service.model.Message;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends MongoRepository<Message, String> {
     Page<Message> findByRoomId(String roomId, Pageable pageable);
+    @Query("{$and: [ {roomId: null}, {$or: [ {senderId: ?0, recipientId: ?1}, {senderId: ?1, recipientId: ?0} ]} ]}")
+    Page<Message> findPrivateConversation(String userId1, String userId2, Pageable pageable);
 }
