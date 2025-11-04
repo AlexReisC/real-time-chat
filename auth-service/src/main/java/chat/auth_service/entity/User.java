a792b52ec1e22378d0319de9cd8302c345cd9364
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-04 11:25:30 -0300

    feat(entity): add User and Role entities with JPA annotations

---
+++ auth-service/src/main/java/chat/auth_service/entity/User.java
@@ -0,0 +1,38 @@
+package chat.auth_service.entity;
+
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 50, nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private Set<Role> roles;
+}
