f3c95dfacb7405b226e578a0d42cb7cd3c202d58
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-04 15:48:46 -0300

    feat(auth-service): create enum for role values in Role entity

--- auth-service/src/main/java/chat/auth_service/entity/Role.java
+++ auth-service/src/main/java/chat/auth_service/entity/Role.java
@@ -1,5 +1,6 @@
 package chat.auth_service.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +12,23 @@ import jakarta.persistence.Table;
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
     private Long roleId;
 
     private String name;
+
+    public enum Values {
+        ADMIN(1L),
+        BASIC(2L);
+
+        Long roleId;
+
+        Values(Long roleId) {
+            this.roleId = roleId;
+        }
+
+        public long getRoleId(){
+            return this.roleId;
+        }
+    }
 }
