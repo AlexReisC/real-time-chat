076ae8775f3a8bb709684f4bf5f25d91446a3598
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-06 18:42:11 -0300

    feat(auth-service): create UserServiceDetailsImpl

---
+++ auth-service/src/main/java/chat/auth_service/service/UserServiceDetailsImpl.java
@@ -0,0 +1,20 @@
+package chat.auth_service.service;
+
+import chat.auth_service.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserServiceDetailsImpl implements UserDetailsService {
+    private final UserRepository repository;
+
+    public UserServiceDetailsImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + username));
+    }
+}
