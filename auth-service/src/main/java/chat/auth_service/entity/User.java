d88aab308e76a90057efb0e49942e57591aebcf9
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-06 22:50:25 -0300

    remove(auth-service): delete Role entity

--- auth-service/src/main/java/chat/auth_service/entity/User.java
+++ auth-service/src/main/java/chat/auth_service/entity/User.java
@@ -44,14 +44,6 @@ public class User implements UserDetails{
     @Column(nullable = false)
     private String password;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    @JoinTable(
-        name = "user_roles",
-        joinColumns = @JoinColumn (name = "user_id"),
-        inverseJoinColumns = @JoinColumn (name = "role_id")
-    )
-    private Set<Role> roles;
-
     @Builder.Default
     @Column(name = "account_non_expired", nullable = false)
     private boolean accountNonExpired = true;
