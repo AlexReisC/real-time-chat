011ac7e26775dd0c93398314a5ba8c97aa6496e9
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-07 13:36:54 -0300

    feat(auth-service): implement doFilterInternal method

--- auth-service/src/main/java/chat/auth_service/config/JwtAuthFilter.java
+++ auth-service/src/main/java/chat/auth_service/config/JwtAuthFilter.java
@@ -1,9 +1,17 @@
 package chat.auth_service.config;
 
+import chat.auth_service.entity.User;
+import chat.auth_service.exception.MissingTokenException;
+import chat.auth_service.service.JwtService;
+import chat.auth_service.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,17 +20,34 @@ import java.util.Arrays;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!checkIfEndpointIsPublic(request)) {
+            String recoveredToken = recoveryToken(request);
+            if (recoveredToken != null) {
+                String username = jwtService.getSubjectFromToken(recoveredToken);
+                User user = userDetailsService.loadUserByUsername(username);
+
+                Authentication authentication = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                throw new MissingTokenException("O token está ausente");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 
     private String recoveryToken(HttpServletRequest request){
         String headerAuth = request.getHeader("Authorization");
-
         if(headerAuth != null){
             return headerAuth.replace("Bearer ", "");
         }
-
         return null;
     }
 
