50c5b22d7d983e31c55457a5804e4996d8182137
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-08 23:42:41 -0300

    feat(auth-service): create handleValidationExceptions method

--- auth-service/src/main/java/chat/auth_service/exception/GlobalExceptionHandler.java
+++ auth-service/src/main/java/chat/auth_service/exception/GlobalExceptionHandler.java
@@ -4,25 +4,38 @@ import chat.auth_service.dto.response.ErrorApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<ErrorApiResponse> handleClientAlreadyExists(UsernameNotFoundException exception){
+    public ResponseEntity<ErrorApiResponse> handleUserNotFound(UsernameNotFoundException exception){
         ErrorApiResponse errorApiResponse = new ErrorApiResponse(
                 "Usuário não encontrado",
                 List.of(exception.getMessage()),
-                HttpStatus.NOT_FOUND.value(),
                 LocalDateTime.now()
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorApiResponse);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorApiResponse> handleValidationExceptions(MethodArgumentNotValidException exception) {
+        List<String> erros = new ArrayList<>();
+        exception.getBindingResult()
+                .getFieldErrors().forEach(er -> erros.add(er.getField() + ": " + er.getDefaultMessage()));
 
+        ErrorApiResponse erroApiResponse = new ErrorApiResponse(
+                "Dados de entrada inválidos",
+                erros,
+                LocalDateTime.now()
+        );
+        return ResponseEntity.badRequest().body(erroApiResponse);
+    }
 }
