4c5c48f9384c9d7534694a0708bcd6be9d3fdb99
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-08 22:53:18 -0300

    feat(auth-service): create login method in AuthController

--- auth-service/src/main/java/chat/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/chat/auth_service/controller/AuthController.java
@@ -1,8 +1,18 @@
 package chat.auth_service.controller;
 
+import chat.auth_service.dto.request.CreateUserDTO;
+import chat.auth_service.dto.request.LoginUserDTO;
+import chat.auth_service.dto.response.RecoveryTokenDTO;
+import chat.auth_service.dto.response.UserResponseDTO;
 import chat.auth_service.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -11,6 +21,12 @@ public class AuthController {
 
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<RecoveryTokenDTO> login(@Valid @RequestBody LoginUserDTO loginUserDTO) {
+        RecoveryTokenDTO tokenDTO = authService.authenticateUser(loginUserDTO);
+        return ResponseEntity.ok(tokenDTO);
     }
 
 }
