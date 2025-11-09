d833b55c976402759e51ac58230b089d0a78090c
AlexReisC <alex.reis@aluno.ufca.edu.br>
2025-11-08 22:52:10 -0300

    feat(auth-service): create EmailAlreadyExistsException

---
+++ auth-service/src/main/java/chat/auth_service/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package chat.auth_service.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+}
