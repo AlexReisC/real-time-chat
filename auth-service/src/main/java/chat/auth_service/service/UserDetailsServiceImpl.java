518588c6e1f064bd44ef3d8925dd5a86bfc02651
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-02-05 09:07:51 -0300

    refactor(auth): simplify UserDetailsServiceImpl by using @RequiredArgsConstructor

--- auth-service/src/main/java/chat/auth_service/service/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/chat/auth_service/service/UserDetailsServiceImpl.java
@@ -2,17 +2,16 @@ package chat.auth_service.service;
 
 import chat.auth_service.entity.User;
 import chat.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository repository;
-
-    public UserDetailsServiceImpl(UserRepository repository) {
-        this.repository = repository;
-    }
 
     @Override
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
