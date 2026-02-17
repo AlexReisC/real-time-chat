6d24a47fdea021067ffb97a916a07b9cb036f5b7
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-02-16 22:01:07 -0300

    fix(chat-service): remove SockJs

--- chat-service/src/main/java/chat/chat_service/websocket/WebSocketConfig.java
+++ chat-service/src/main/java/chat/chat_service/websocket/WebSocketConfig.java
@@ -46,7 +46,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat") // endpoint for connection websocket
                 .addInterceptors(userHandshakeInterceptor)
-                .setAllowedOrigins("*") // allows cors
-                .withSockJS();
+                .setAllowedOriginPatterns("*"); // allows cors
+                // .withSockJS();
     }
 }
