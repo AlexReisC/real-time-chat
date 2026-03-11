ee0568c47d064984ab87718c3316ca03ccd30c93
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-11 16:25:04 -0300

    fix(auth-service): implement hashcode and equals and prevent nullpointerexception by return empty list in getAuthorities

--- auth-service/src/main/java/chat/auth_service/entity/User.java
+++ auth-service/src/main/java/chat/auth_service/entity/User.java
@@ -1,6 +1,7 @@
 package chat.auth_service.entity;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -14,15 +15,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "users")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Getter
+@Setter
+@ToString
 public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -55,7 +60,7 @@ public class User implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of();
     }
 
     @Override
@@ -77,4 +82,30 @@ public class User implements UserDetails{
     public boolean isEnabled() {
         return this.enabled;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
 }
