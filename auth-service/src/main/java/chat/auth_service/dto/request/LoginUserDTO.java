b5ccccf69bc07dde8da09ec3e53e9df2fef6b18f
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-11 16:56:15 -0300

    feat(auth-service): add email format validation in LoginUserDTO

--- auth-service/src/main/java/chat/auth_service/dto/request/LoginUserDTO.java
+++ auth-service/src/main/java/chat/auth_service/dto/request/LoginUserDTO.java
@@ -1,9 +1,11 @@
 package chat.auth_service.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginUserDTO(
         @NotBlank(message = "O email é obrigatório")
+        @Email(message = "Formato de email inválido")
         String email,
 
         @NotBlank(message = "A senha é obrigatória")
