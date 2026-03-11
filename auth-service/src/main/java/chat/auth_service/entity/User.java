924fdf266ae9d52c678566a4a51c93c19b3f5bd7
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-11 16:28:15 -0300

    refactor(auth-service): delegate the equals and hashcode implementation to Spring

--- auth-service/src/main/java/chat/auth_service/entity/User.java
+++ auth-service/src/main/java/chat/auth_service/entity/User.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -27,10 +28,13 @@ import lombok.ToString;
 @Builder
 @Getter
 @Setter
-@ToString
+@ToString(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @EqualsAndHashCode.Include
+    @ToString.Include
     private UUID id;
 
     @Column(length = 50, nullable = false)
@@ -81,31 +85,6 @@ public class User implements UserDetails{
     @Override
     public boolean isEnabled() {
         return this.enabled;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
     }
     
 }
