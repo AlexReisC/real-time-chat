d8cc39fac44c989bdab4a6ea7f902ce1f30339e1
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-12 21:55:54 -0300

    feat(auth-service): update AuthController to use AuthTokenDTO and add refresh method

--- auth-service/src/main/java/chat/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/chat/auth_service/controller/AuthController.java
@@ -2,10 +2,12 @@ package chat.auth_service.controller;
 
 import chat.auth_service.dto.request.CreateUserDTO;
 import chat.auth_service.dto.request.LoginUserDTO;
+import chat.auth_service.dto.request.RefreshRequest;
 import chat.auth_service.dto.response.AuthTokenDTO;
-import chat.auth_service.dto.response.UserResponseDTO;
 import chat.auth_service.service.AuthService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,22 +18,25 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthTokenDTO> register(@Valid @RequestBody CreateUserDTO createUserDTO) {
+        AuthTokenDTO responseDTO = authService.createUser(createUserDTO);
+        return ResponseEntity.created(URI.create("api/v1/auth/register")).body(responseDTO);
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthTokenDTO> login(@Valid @RequestBody LoginUserDTO loginUserDTO) {
-        AuthTokenDTO tokenDTO = authService.authenticateUser(loginUserDTO);
+        AuthTokenDTO tokenDTO = authService.login(loginUserDTO);
         return ResponseEntity.ok(tokenDTO);
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<UserResponseDTO> register(@Valid @RequestBody CreateUserDTO createUserDTO) {
-        UserResponseDTO responseDTO = authService.createUser(createUserDTO);
-        return ResponseEntity.created(URI.create("/auth/register")).body(responseDTO);
+    @PostMapping("/refresh")
+    public ResponseEntity<AuthTokenDTO> refresh(@RequestBody RefreshRequest body) {
+        AuthTokenDTO tokenDTO = authService.refresh(body.refreshToken());
+        return ResponseEntity.ok(tokenDTO);
     }
 }
