8a87170282f78ed7086553e5dd9cd0b85214db09
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-12 21:27:48 -0300

    feat(auth-service): create new migration for user_roles table

--- auth-service/src/main/resources/db/migration/V02__create_correct_user_table.sql
+++ auth-service/src/main/resources/db/migration/V02__create_correct_user_table.sql
@@ -4,8 +4,5 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    account_non_expired BOOLEAN NOT NULL DEFAULT true,
-    account_non_locked BOOLEAN NOT NULL DEFAULT true,
-    credentials_non_expired BOOLEAN NOT NULL DEFAULT true,
     enabled BOOLEAN NOT NULL DEFAULT true
 )
