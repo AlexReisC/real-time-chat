b97c503125d3df82ce46c6bb870f215b1ea8143d
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-15 17:40:03 -0300

    feat(chat-service): adds event listener for when users leaves the room

---
+++ chat-service/src/main/java/chat/chat_service/websocket/WebSocketEventListener.java
@@ -0,0 +1,41 @@
+package chat.chat_service.websocket;
+
+import java.time.Instant;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import chat.chat_service.dto.response.UserNotificationResponseDTO;
+import chat.chat_service.model.NotificationType;
+import chat.chat_service.service.RoomService;
+
+@Component
+public class WebSocketEventListener {
+    private final RoomService roomService;
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public WebSocketEventListener(RoomService roomService, SimpMessagingTemplate messagingTemplate) {
+        this.roomService = roomService;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String userId = (String) headerAccessor.getSessionAttributes().get("userId");
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
+
+        if (userId != null) {
+            roomService.removeUser(roomId, userId);
+            
+            UserNotificationResponseDTO response = new UserNotificationResponseDTO(
+                    NotificationType.LEAVE, userId, username, roomId, username + " saiu da sala (desconectado)", Instant.now()
+            );
+            messagingTemplate.convertAndSend("/topic/rooms." + roomId, response);
+        }
+    }
+}
