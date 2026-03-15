5c86d6f4c3344bae5561cecfc3f9e0dbd3b1fd4e
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-15 17:44:14 -0300

    fix(chat-service): correct endpoints for messages

--- chat-service/src/main/java/chat/chat_service/controller/MessageController.java
+++ chat-service/src/main/java/chat/chat_service/controller/MessageController.java
@@ -10,11 +10,11 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import chat.chat_service.dto.request.ConversationSummaryDTO;
 import chat.chat_service.dto.response.PageResponseDTO;
 import chat.chat_service.dto.response.ResponseMessageDTO;
 import chat.chat_service.service.MessageService;
@@ -32,42 +32,40 @@ public class MessageController {
         this.messageService = messageService;
     }
 
-    @GetMapping("/room/{roomId}")
+    @GetMapping("/room/{roomId}/history")
     public ResponseEntity<PageResponseDTO<ResponseMessageDTO>> listAllMessagesByRoom(
             @PathVariable @NotBlank(message = "O ID da sala é obrigatório") String roomId,
-            @Min(value = 0) @Max(value = 100) @RequestParam(defaultValue = "0") int page,
-            @Min(value = 1) @Max(value = 100) @RequestParam(defaultValue = "10") int size,
+            @Min(value = 0) @Max(value = 100) @RequestParam(defaultValue = "1") int page,
+            @Min(value = 1) @Max(value = 100) @RequestParam(defaultValue = "50") int size,
             @RequestParam(defaultValue = "timestamp") String sortBy
-            ) {
+    ) {
         PageResponseDTO<ResponseMessageDTO> allMessages = messageService.listAllMessages(roomId, PageRequest.of(page, size, Sort.by(sortBy)));
         return ResponseEntity.ok(allMessages);
     }
 
-    @GetMapping("/private")
+    @GetMapping("/private/{targetUserId}/history")
     public ResponseEntity<PageResponseDTO<ResponseMessageDTO>> listAllPrivateMessages (
-            @RequestParam @NotBlank(message = "O ID do usuário 1 é obrigatório") String targetUserId,
+            @PathVariable String targetUserId,
             @AuthenticationPrincipal Jwt jwt,
-            @Min(value = 0) @Max(value = 100) @RequestParam(defaultValue = "0") int page,
-            @Min(value = 1) @Max(value = 100) @RequestParam(defaultValue = "10") int size,
+            @Min(value = 0) @Max(value = 100) @RequestParam(defaultValue = "1") int page,
+            @Min(value = 1) @Max(value = 100) @RequestParam(defaultValue = "50") int size,
             @RequestParam(defaultValue = "timestamp") String sortBy
-            ) {
-        String currentUserId = jwt.getSubject();
+    ) {
+        String currentUserId = jwt.getClaimAsString("userId");
         PageRequest pageRequest = PageRequest.of(page, size, Sort.by(sortBy));
         PageResponseDTO<ResponseMessageDTO> allPrivateMessages = messageService.listAllPrivateMessages(currentUserId, targetUserId, pageRequest);
         return ResponseEntity.ok(allPrivateMessages);
     }
 
-    @GetMapping("/room/{roomId}/messages")
+    @GetMapping("/room/{roomId}")
     public ResponseEntity<List<ResponseMessageDTO>> getRoomHistory(@NotBlank(message = "O ID da sala é obrigatório") @PathVariable String roomId) {
         List<ResponseMessageDTO> history = messageService.getRecentRoomMessages(roomId);
         return ResponseEntity.ok(history);
     }
 
     @GetMapping("/private/{targetUserId}")
-    public ResponseEntity<List<ResponseMessageDTO>> getPrivateMessages(
-        @NotBlank(message = "O ID do usuário é obrigatório") @PathVariable String targetUserId,
-        @RequestAttribute("userId") String currentUserId
-    ) {
+    public ResponseEntity<List<ResponseMessageDTO>> getPrivateMessages(@PathVariable String targetUserId, @AuthenticationPrincipal Jwt jwt) {
+        String currentUserId = jwt.getClaimAsString("userId");
         List<ResponseMessageDTO> history = messageService.getRecentPrivateMessages(targetUserId, currentUserId);
         return ResponseEntity.ok(history);
     }
