4a9eb6889db07bca19ab916dd6b9db8acbebac07
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-03-15 14:18:03 -0300

    remove(fronted): delete frontend

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from './context/AuthContext';
-import { LoginPage } from './pages/LoginPage';
-import { ChatPage } from './pages/ChatPage';
-import './styles/globals.css';
-
-export default function App() {
-  const { user } = useAuth();
-  return user ? <ChatPage /> : <LoginPage />;
-}
