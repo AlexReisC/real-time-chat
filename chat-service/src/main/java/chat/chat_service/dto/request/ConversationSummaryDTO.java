3d27aea2e94a2ce6a2cf6a5f59062fab304c2e69
AlexReisC <alex.reis@aluno.ufca.edu.br>
2026-04-12 17:50:29 -0300

    refactor(chat-service): rename fields for consistency with user model

--- chat-service/src/main/java/chat/chat_service/dto/request/ConversationSummaryDTO.java
+++ chat-service/src/main/java/chat/chat_service/dto/request/ConversationSummaryDTO.java
@@ -3,8 +3,8 @@ package chat.chat_service.dto.request;
 import java.time.Instant;
 
 public record ConversationSummaryDTO(
-        String contactId,
-        String contactUsername,
+        String userId,
+        String username,
         String lastMessage,
         Instant timestamp
 ) { }
